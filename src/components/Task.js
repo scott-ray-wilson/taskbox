b03092532071a9f51c1023a800a159bbf46cc13a
Scott Wilson <scottraywilson@gmail.com>
2021-06-27 16:42:49 -0700

    add text overflow

--- src/components/Task.js
+++ src/components/Task.js
@@ -14,7 +14,7 @@ export default function Task({task: {id, title, state}, onArchiveTask, onPinTask
                 <span className="checkbox-custom" onClick={() => onArchiveTask(id)}/>
             </label>
             <div className="title">
-                <input style={{ background: 'red'}} type="text" value={title} readOnly={true} placeholder="Input title"/>
+                <input style={{ textOverflow: 'ellipsis' }} type="text" value={title} readOnly={true} placeholder="Input title"/>
             </div>
 
             <div className="actions" onClick={event => event.stopPropagation()}>
